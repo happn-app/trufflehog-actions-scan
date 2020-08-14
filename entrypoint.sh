6e01a24a36e35b81b59b788f8b4009e83b75cdbd
davy Radionoff <davy.radionoff@happn.com>
2020-08-14 12:33:20 +0200

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
 
-if [ -n "${INPUT_GITHUBTOKEN}" ]; then
-  githubRepo="https://$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
+if [ -n "${INPUT_GCLOUD_SERVICE_KEY}" ]; then
+  githubRepo="https://$INPUT_GCLOUD_SERVICE_KEY@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
 else
   githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
 fi
