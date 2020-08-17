a0caa392e3308bfd52c7221872c43aa1dc67466a
davy Radionoff <davy.radionoff@happn.com>
2020-08-17 10:29:23 +0200

    add input token

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,11 @@ if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
 
-githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
+if [ -n "${INPUT_GITHUBTOKEN}" ]; then
+  githubRepo="https://$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
+else
+  githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
+fi
 
 query="$args $githubRepo" # Build args query with repository url
 trufflehog $query
