a4ee7b0ef784bdc1003174d2f315c6d885867a6a
François Lamboley <Frizlab@users.noreply.github.com>
2023-04-13 18:37:55 +0200

    Update entrypoint.sh
    Output any output of the script to log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env sh
-set -e # Abort script at first error
+set -eu
 
-args="--regex --entropy=False --max_depth=50 --json" # Default trufflehog options
+LOGFILE=trufflehog_results.txt
+
+# Redirect ANY output from this script to "$LOGFILE"
+exec 1<&-;          exec 2<&-
+exec 1<>"$LOGFILE"; exec 2>&1
+
+# Default trufflehog options
+args="--regex --entropy=False --max_depth=50 --json"
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
 
 if [ -n "${INPUT_GITHUBTOKEN}" ]; then
-  githubRepo="https:///x-access-token:$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY.git" # Overwrite for private repository if token provided
+  # Overwrite for private repository if token provided
+  githubRepo="https:///x-access-token:$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY.git"
 else
   githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
 fi
 
 query="$args $githubRepo" # Build args query with repository url
-trufflehog $query > trufflehog_results.txt
+trufflehog $query >./trufflehog_results.txt 2>&1
